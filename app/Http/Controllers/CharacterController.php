2989c655ad3b8da7ae5eb9d6527ea553ad72a114
Rafael Souza <rafaelomrac@gmail.com>
2026-05-24 17:12:48 -0300

    atualizando charactercontroller

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class CharacterController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $response = Http::get('https://rickandmortyapi.com/api/character');
+        $response = Http::get('https://rickandmortyapi.com/api/character', $request->only([
+            'page', 'name', 'status', 'species', 'type', 'gender'
+        ]));
+
+        if ($response->failed()) {
+            return response()->json(['error' => 'Falha ao buscar personagens'], 502);
+        }
 
         return response()->json($response->json());
     }
